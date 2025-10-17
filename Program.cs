b411abd1e560bc1bfe34f30f14d42d55a1f30d06
Cesar Jose Godoy <cesarjosegodoy@gmail.com>
2025-10-17 18:52:31 -0300

    fin tp

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.Data.Sqlite;
-string connectionString = "Data Source=base_test.db;";
+string connectionString = "Data Source=tienda.db;";
 
 // Crear conexión a la base de datos
 using (SqliteConnection connection = new SqliteConnection(connectionString))
@@ -8,29 +8,29 @@ using (SqliteConnection connection = new SqliteConnection(connectionString))
     // Crear tabla si no existe
     // por lo general este tipo de consultas no se implementa en un porgrama real
     // la aplicamos para poder crear nuestra base de datos desde cero
-    string createTableQuery = "CREATE TABLE IF NOT EXISTS productos (id INTEGER PRIMARY KEY, nombre TEXT, precio REAL)";
+   /* string createTableQuery = "CREATE TABLE IF NOT EXISTS productos (id INTEGER PRIMARY KEY, nombre TEXT, precio REAL)";
     using (SqliteCommand createTableCmd = new SqliteCommand(createTableQuery, connection))
     {
         createTableCmd.ExecuteNonQuery();
         Console.WriteLine("Tabla 'productos' creada o ya existe.");
-    }
+    }*/
     
     // Insertar datos
-    string insertQuery = "INSERT INTO productos (nombre, precio) VALUES ('Manzana', 0.50), ('Banana', 0.30)";
+    string insertQuery = "INSERT INTO Productos (Descripcion, Precio) VALUES ('Manzana', 0.50), ('Banana', 0.30)";
             using (SqliteCommand insertCmd = new SqliteCommand(insertQuery, connection))
             {
                 insertCmd.ExecuteNonQuery();
                 Console.WriteLine("Datos insertados en la tabla 'productos'.");
             }
     // Leer datos
-            string selectQuery = "SELECT * FROM productos";
+            string selectQuery = "SELECT * FROM Presupuestos";
             using (SqliteCommand selectCmd = new SqliteCommand(selectQuery, connection))
             using (SqliteDataReader reader = selectCmd.ExecuteReader())
             {
-                Console.WriteLine("Datos en la tabla 'productos':");
+                Console.WriteLine("Datos en la tabla 'Presupuestos':");
                 while (reader.Read())
                 {
-                    Console.WriteLine($"ID: {reader["id"]}, Nombre: {reader["nombre"]}, Precio: {reader["precio"]}");
+                    Console.WriteLine($"ID: {reader["IdPresupuesto"]}, Nombre: {reader["NombreDestinatario"]}, Date: {reader["FechaCreacion"]}");
                 }
             }
 
